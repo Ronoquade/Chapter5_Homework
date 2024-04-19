37e9be7f6d36628c5f81e06b1da22b2fe1dbf0b1
Ronoquade Lawrencea <rlawrence2@ggc.edu>
2024-04-18 20:30:32 -0400

    initial commit

--- src/Exercise3/Exercise3.java
+++ src/Exercise3/Exercise3.java
@@ -8,15 +8,21 @@ import java.util.Scanner;
 public class Exercise3 {
     public static void main(String[] args) throws IOException {
         File file = new File("src/Exercise3/poem2.txt");
-        Poem[] poems = {new Poem(), new Poem(), new Poem()};
+        Poem[] poems = new Poem[4];
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String data;
-            for(Poem p: poems) {
                 while((data = br.readLine()) != null) {
-                    p.setName(data);
-
+                    for(int i = 0; i < 4; i++) {
+                        poems[i] = new Poem();
+                    }
+                    for(Poem p: poems) {
+                        p.setName(data);
+                        data = br.readLine();
+                        p.setPoet(data);
+                        System.out.printf("The name of the poem: %s\nThe name of the poet: %s\n\n", p.getName(),p.getPoet());
+                        data = br.readLine();
+                    }
                 }
-            }
         }
         catch (IOException e) {
             e.printStackTrace();
